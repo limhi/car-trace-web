094bb902aaf67add53cb75099f57535a1b0b2929
limhi.cpc@gmail.com <limhi.cpc@gmail.com@c5c1879a-c7a8-87fb-ce67-8c74e476de1a>
2013-09-23 17:49:56 +0000

    新增產生亂碼認證的api

---
+++ trunk/src/org/luke/ct/dao/CarPhoneRandomNumberService.java
@@ -0,0 +1,8 @@
+package org.luke.ct.dao;
+
+import org.luke.ct.core.IDAO;
+import org.luke.ct.model.CarPhoneRandomNumber;
+
+public interface CarPhoneRandomNumberService extends IDAO<CarPhoneRandomNumber> {
+
+}
