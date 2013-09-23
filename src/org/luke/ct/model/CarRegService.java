c33954021cbb4d2313a8ede652fb629a3402dde2
limhi.cpc@gmail.com <limhi.cpc@gmail.com@c5c1879a-c7a8-87fb-ce67-8c74e476de1a>
2013-09-23 17:49:29 +0000

    reorg

--- src/org/luke/ct/model/CarRegService.java
+++
@@ -1,7 +0,0 @@
-package org.luke.ct.model;
-
-import org.luke.ct.core.IDAO;
-
-public interface CarRegService extends IDAO<CarReg> {
-
-}
--- src/org/luke/ct/model/CarRegServiceImpl.java
+++
@@ -1,6 +0,0 @@
-package org.luke.ct.model;
-
-import org.luke.ct.core.DAOSupport;
-
-public class CarRegServiceImpl extends DAOSupport<CarReg> implements CarRegService {
-}
