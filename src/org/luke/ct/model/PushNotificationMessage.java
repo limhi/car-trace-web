3cef342c2b16dab82caca0ee5df8f52252d3bfc4
limhi.cpc@gmail.com <limhi.cpc@gmail.com@c5c1879a-c7a8-87fb-ce67-8c74e476de1a>
2013-09-25 17:27:55 +0000

    新增 對car 及 phone的push notification message的提供 web api

---
+++ src/org/luke/ct/model/PushNotificationMessage.java
@@ -0,0 +1,67 @@
+package org.luke.ct.model;
+
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+import com.google.api.server.spi.config.AnnotationBoolean;
+import com.google.api.server.spi.config.ApiSerializationProperty;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+
+@PersistenceCapable(detachable = "true")
+public class PushNotificationMessage {
+  @PrimaryKey
+  @Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+  @ApiSerializationProperty(ignored = AnnotationBoolean.TRUE)
+  private Key key;
+  @Persistent
+  private String encodedKey;
+  @Persistent
+  private String type;
+  @Persistent
+  private String message;
+  @Persistent
+  private String addTime;
+
+  public PushNotificationMessage() {
+  }
+
+  public Key getKey() {
+    return key;
+  }
+
+  public void setKey(Key key) {
+    this.key = key;
+  }
+
+  public String getEncodedKey() {
+    return KeyFactory.keyToString(key);
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+
+  public String getAddTime() {
+    return addTime;
+  }
+
+  public void setAddTime(String addTime) {
+    this.addTime = addTime;
+  }
+
+}
