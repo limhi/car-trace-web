1e3b6860e3d780c35b34c6ebb1bf9a143b974a77
limhi.cpc@gmail.com <limhi.cpc@gmail.com@c5c1879a-c7a8-87fb-ce67-8c74e476de1a>
2013-09-29 16:36:34 +0000

    set up timezone to taipei

--- trunk/src/org/luke/ct/core/CTCommon.java
+++ trunk/src/org/luke/ct/core/CTCommon.java
@@ -6,15 +6,15 @@ import java.util.Calendar;
 public class CTCommon {
   public static String getNowTime() {
     Calendar nowTime = Calendar.getInstance();
-    nowTime.add(Calendar.MINUTE, 60 * 24 * 8);
+    nowTime.add(Calendar.HOUR_OF_DAY, 8);
     return new SimpleDateFormat("yyyyMMddHHmmssSSS").format(nowTime.getTime());
   }
 
   public static String getDeadTime(int unit, int amount) {
     Calendar nowTime = Calendar.getInstance();
     nowTime.add(unit, amount);
-    
-    nowTime.add(Calendar.MINUTE, 60 * 24 * 8);
+
+    nowTime.add(Calendar.HOUR_OF_DAY, 8);
     return new SimpleDateFormat("yyyyMMddHHmmssSSS").format(nowTime.getTime());
   }
 }
