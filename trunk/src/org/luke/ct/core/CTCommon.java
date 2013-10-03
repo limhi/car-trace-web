c71d9d9da5931879b7d3cd6076205fda59d06495
limhi.cpc@gmail.com <limhi.cpc@gmail.com@c5c1879a-c7a8-87fb-ce67-8c74e476de1a>
2013-10-03 16:08:10 +0000

    新增push notification的http client呼叫

--- trunk/src/org/luke/ct/core/CTCommon.java
+++ trunk/src/org/luke/ct/core/CTCommon.java
@@ -4,17 +4,22 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 public class CTCommon {
-  public static String getNowTime() {
-    Calendar nowTime = Calendar.getInstance();
-    nowTime.add(Calendar.HOUR_OF_DAY, 8);
-    return new SimpleDateFormat("yyyyMMddHHmmssSSS").format(nowTime.getTime());
-  }
+	public static final String API_KEY = "AIzaSyDvBTbuUfpGdaNehyjiZ8MqU2phTc9jS0w";
+	public static final String GCM_URL = "https://android.googleapis.com/gcm/send";
 
-  public static String getDeadTime(int unit, int amount) {
-    Calendar nowTime = Calendar.getInstance();
-    nowTime.add(unit, amount);
+	public static String getNowTime() {
+		Calendar nowTime = Calendar.getInstance();
+		nowTime.add(Calendar.HOUR_OF_DAY, 8);
+		return new SimpleDateFormat("yyyyMMddHHmmssSSS").format(nowTime
+				.getTime());
+	}
 
-    nowTime.add(Calendar.HOUR_OF_DAY, 8);
-    return new SimpleDateFormat("yyyyMMddHHmmssSSS").format(nowTime.getTime());
-  }
+	public static String getDeadTime(int unit, int amount) {
+		Calendar nowTime = Calendar.getInstance();
+		nowTime.add(unit, amount);
+
+		nowTime.add(Calendar.HOUR_OF_DAY, 8);
+		return new SimpleDateFormat("yyyyMMddHHmmssSSS").format(nowTime
+				.getTime());
+	}
 }
