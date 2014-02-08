8dfeecce146d59759c885c9d0e1d48844acecd5e
limhi.cpc@gmail.com <limhi.cpc@gmail.com@c5c1879a-c7a8-87fb-ce67-8c74e476de1a>
2014-02-08 15:48:58 +0000

    新增記錄上傳圖片的db api

---
+++ trunk/src/org/luke/ct/dao/CarUploadService.java
@@ -0,0 +1,8 @@
+package org.luke.ct.dao;
+
+import org.luke.ct.core.IDAO;
+import org.luke.ct.model.CarUpload;
+
+public interface CarUploadService extends IDAO<CarUpload> {
+
+}
---
+++ trunk/src/org/luke/ct/dao/CarUploadServiceImpl.java
@@ -0,0 +1,7 @@
+package org.luke.ct.dao;
+
+import org.luke.ct.core.DAOSupport;
+import org.luke.ct.model.CarUpload;
+
+public class CarUploadServiceImpl extends DAOSupport<CarUpload> implements CarUploadService {
+}
